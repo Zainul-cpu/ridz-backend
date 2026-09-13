72adc26231f10ba166a735627345e5f03e3924c2
Ridz Storey <ridwanzainul811@gmail.com>
2026-09-13 16:20:27 +0700

    Add CORS support

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -45,8 +47,8 @@ app.post("/api/order", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
 });
